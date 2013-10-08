874cf9da468bdbc0d20656172dd086c737bf0329
chenxy <chenxy@polyvi.com>
2013-10-08 11:25:44 +0800

    将wp8改为wp，同时将路径www改为xface3/helloxface
    以后需要根据实际应用id进行处理

--- src/platforms.js
+++ src/platforms.js
@@ -3,7 +3,7 @@ module.exports = {
     'ios': require('./platforms/ios'),
     'blackberry10': require('./platforms/blackberry10'),
     'wp7': require('./platforms/wp7'),
-    'wp8': require('./platforms/wp8'),
+    'wp': require('./platforms/wp'),
     'windows8' : require('./platforms/windows8'),
     'firefoxos': require('./platforms/firefoxos')
 };
--- src/prepare.js
+++ src/prepare.js
@@ -22,7 +22,7 @@ var platform_modules = require('./platforms'),
     config_changes  = require('./util/config-changes'),
     xml_helpers     = require('./util/xml-helpers'),
     wp7             = require('./platforms/wp7'),
-    wp8             = require('./platforms/wp8'),
+    wp              = require('./platforms/wp'),
     windows8        = require('./platforms/windows8'),
     fs              = require('fs'),
     shell           = require('shelljs'),
@@ -71,15 +71,15 @@ module.exports = function handlePrepare(project_dir, platform, plugins_dir) {
     // for windows phone plaform we need to add all www resources to the .csproj file
     // first we need to remove them all to prevent duplicates
     var wp_csproj;
-    if(platform == 'wp7' || platform == 'wp8') {
-        wp_csproj = (platform == wp7? wp7.parseProjectFile(project_dir) : wp8.parseProjectFile(project_dir));
+    if(platform == 'wp7' || platform == 'wp') {
+        wp_csproj = (platform == wp7? wp7.parseProjectFile(project_dir) : wp.parseProjectFile(project_dir));
         var item_groups = wp_csproj.xml.findall('ItemGroup');
         for (var i = 0, l = item_groups.length; i < l; i++) {
             var group = item_groups[i];
             var files = group.findall('Content');
             for (var j = 0, k = files.length; j < k; j++) {
                 var file = files[j];
-                if (file.attrib.Include.substr(0,11) == "www\\plugins" || file.attrib.Include == "www\\cordova_plugins.js") {
+                if (file.attrib.Include.substr(0,11) == "xface3\\helloxface\\plugins" || file.attrib.Include == "xface3\\helloxface\\cordova_plugins.js") {
                     // remove file reference
                     group.remove(0, file);
                     // remove ItemGroup if empty
@@ -161,8 +161,8 @@ module.exports = function handlePrepare(project_dir, platform, plugins_dir) {
                 var scriptContent = fs.readFileSync(path.join(pluginDir, fsPath), 'utf-8');
                 scriptContent = 'cordova.define("' + moduleName + '", function(require, exports, module) {' + scriptContent + '});\n';
                 fs.writeFileSync(path.join(platformPluginsDir, plugin_id, fsPath), scriptContent, 'utf-8');
-                if(platform == 'wp7' || platform == 'wp8' || platform == "windows8") {
-                    wp_csproj.addSourceFile(path.join('www', 'plugins', plugin_id, fsPath));
+                if(platform == 'wp7' || platform == 'wp' || platform == "windows8") {
+                    wp_csproj.addSourceFile(path.join('xface3', 'helloxface', 'plugins', plugin_id, fsPath));
                 }
     
                 // Prepare the object for cordova_plugins.json.
@@ -200,8 +200,8 @@ module.exports = function handlePrepare(project_dir, platform, plugins_dir) {
     require('../plugman').emit('verbose', 'Writing out cordova_plugins.js...');
     fs.writeFileSync(path.join(wwwDir, 'cordova_plugins.js'), final_contents, 'utf-8');
 
-    if(platform == 'wp7' || platform == 'wp8' || platform == "windows8") {
-        wp_csproj.addSourceFile(path.join('www', 'cordova_plugins.js'));
+    if(platform == 'wp7' || platform == 'wp' || platform == "windows8") {
+        wp_csproj.addSourceFile(path.join('xface3', 'helloxface', 'cordova_plugins.js'));
         wp_csproj.write();
     }
 };
