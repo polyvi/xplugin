7827b5b93adefb9ee1131357b6e5ce7ab323b649
chenxy <chenxy@polyvi.com>
2013-10-09 10:37:30 +0800

    将windows phone平台名称由wp改回wp8

--- src/platforms.js
+++ src/platforms.js
@@ -3,7 +3,7 @@ module.exports = {
     'ios': require('./platforms/ios'),
     'blackberry10': require('./platforms/blackberry10'),
     'wp7': require('./platforms/wp7'),
-    'wp': require('./platforms/wp'),
+    'wp8': require('./platforms/wp8'),
     'windows8' : require('./platforms/windows8'),
     'firefoxos': require('./platforms/firefoxos')
 };
